c158b1c370860592717d6216144f2e5bf72b5dd1
rsavoj <rsavoj@ucsd.edu>
2023-12-07 10:05:16 -0800

    Update rating_widget.js

--- rating_widget.js
+++ rating_widget.js
@@ -2,7 +2,7 @@ class ratingWidget extends HTMLElement {
     constructor(){
         super();
         this.attachShadow({mode: 'open'})
-       
+        this.shadowRoot.innerHTML = '<h2>Ratings Widget</h2>'
 
     }
 }
