11965610a373e48a0c614ff2646c78e199d3fbf9
rsavoj <rsavoj@ucsd.edu>
2023-12-07 11:26:09 -0800

    Update rating_widget.js

--- rating_widget.js
+++ rating_widget.js
@@ -4,11 +4,11 @@ class RatingWidget extends HTMLElement {
         this.attachShadow({mode: 'open'})
         this.shadowRoot.innerHTML = `
 				<style>
-                    .colored {
+                    .Colored {
                         font-size: 5rem;
                         color: red;
                     }
-                    .notColored {
+                    span {
                         font-size: 2rem;
                         color: orange;
                     }
@@ -19,21 +19,21 @@ class RatingWidget extends HTMLElement {
         this.star1 = document.createElement('span');
         this.star1.innerHTML = '&star;'; 
         this.star1.id = 'star1';
-        //this.star1.className = 'notColored'; 
+
         
         const star2 = document.createElement('span');
         star2.innerHTML = '&star;'; 
         star2.id = 'star2';
-        star2.className = 'notColored';  
+         
 
         const star3 = document.createElement('span');
         star3.innerHTML = '&star;'; 
         star3.id = 'star3';  
-        star3.className = 'notColored'; 
+
         const star4 = document.createElement('span');
         star4.innerHTML = '&star;'; 
         star4.id = 'star4';
-        star4.className = 'notColored';   
+  
         const star5 = document.createElement('span');
         star5.innerHTML = '&star;'; 
         star5.id = 'star5';  
