0360df4747e079e8e4eb8b3efd0ef05c3c23accd
rsavoj <rsavoj@ucsd.edu>
2023-12-07 10:24:18 -0800

    Update rating_widget.js

--- rating_widget.js
+++ rating_widget.js
@@ -5,10 +5,31 @@ class ratingWidget extends HTMLElement {
         const heading = document.createElement('h2');
         heading.textContent = 'Ratings Widget';
 
-        const star = document.createElement('span');
+        const star1 = document.createElement('span');
         star.innerHTML = '&star;'; 
+        heartSpan.id = 'star1';
+        
+        const star2 = document.createElement('span');
+        star.innerHTML = '&star;'; 
+        heartSpan.id = 'star2';  
+
+        const star3 = document.createElement('span');
+        star.innerHTML = '&star;'; 
+        heartSpan.id = 'star3';  
+        const star4 = document.createElement('span');
+        star.innerHTML = '&star;'; 
+        heartSpan.id = 'star4';  
+        const star5 = document.createElement('span');
+        star.innerHTML = '&star;'; 
+        heartSpan.id = 'star5';  
+
+      
         this.shadowRoot.appendChild(heading);
-        this.shadowRoot.appendChild(star);
+        this.shadowRoot.appendChild(star1);
+        this.shadowRoot.appendChild(star2);
+        this.shadowRoot.appendChild(star3);
+        this.shadowRoot.appendChild(star4);
+        this.shadowRoot.appendChild(star5);
 
     }
 }
