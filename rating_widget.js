bca4ae1bef132f5391e5014e01d384fddbb1f44e
rsavoj <rsavoj@ucsd.edu>
2023-12-07 11:10:24 -0800

    Update rating_widget.js

--- rating_widget.js
+++ rating_widget.js
@@ -38,12 +38,12 @@ class ratingWidget extends HTMLElement {
         star5.innerHTML = '&star;'; 
         star5.id = 'star5';  
         star1.className = 'colored'; 
-        this.shadowRoot.appendChild(heading);
-        this.shadowRoot.appendChild(star1);
-        this.shadowRoot.appendChild(star2);
-        this.shadowRoot.appendChild(star3);
-        this.shadowRoot.appendChild(star4);
-        this.shadowRoot.appendChild(star5);
+        this.shadowRoot.appendChild(this.heading);
+        this.shadowRoot.appendChild(this.star1);
+        this.shadowRoot.appendChild(this.star2);
+        this.shadowRoot.appendChild(this.star3);
+        this.shadowRoot.appendChild(this.star4);
+        this.shadowRoot.appendChild(this.star5);
 
     }
 }
