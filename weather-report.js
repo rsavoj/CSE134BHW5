b04d5297c2b5a7ba7e064e92e176beef121a9518
rsavoj <rsavoj@ucsd.edu>
2023-12-07 18:43:22 -0800

    Update weather-report.js

--- weather-report.js
+++ weather-report.js
@@ -13,8 +13,28 @@ class WeatherReport extends HTMLElement {
                         color: red;
                     }
                 </style>`;
-            const output = document.querySelector('output');
-            const button = document.querySelector('button');
+            const output = this.shadowRoot.querySelector('output');
+            const button = this.shadowRoot.querySelector('button');
+            button.addEventListener('click', () => {
+                // Call the fetch API
+                fetch('https://worldtimeapi.org/api/ip')
+                    .then(response => {
+                        // Check if the response is ok (status in the range 200-299)
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        // Display the time
+                        output.textContent = data.datetime;
+                    })
+                    .catch(error => {
+                        // Handle any errors
+                        console.error('Fetch error:', error);
+                        output.textContent = 'Error fetching time';
+                    });
+            });
 
        
       
