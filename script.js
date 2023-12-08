a00012f39479c0dfa4dabb35d0ccf11193d1eb5a
rsavoj <rsavoj@ucsd.edu>
2023-12-07 18:00:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
 
+import { createReadStream } from 'fs';
 var fs = require('fs');
 
-var readStream = fs.createReadStream('./demofile.txt');
+var readStream = createReadStream('./demofile.txt');
 
 /*Write to the console when the file is opened:*/
-readStream.on('open', function () {
-  console.log('The file is open');
-});
+readStream.on('open', () => {
+        console.log('The file is open');
+    });
 
               
 
