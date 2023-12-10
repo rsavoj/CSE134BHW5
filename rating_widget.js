04fc012e0ef4f4f095cdc2f4551e55512f9e771a
rsavoj <rsavoj@ucsd.edu>
2023-12-09 21:18:28 -0800

    Update rating_widget.js

--- rating_widget.js
+++ rating_widget.js
@@ -89,7 +89,7 @@ class RatingWidget extends HTMLElement {
     handleResponse(xhr){
         if(xhr.readyState == 4 && xhr.status == 200){
             var responce = xhr.responseXML;
-            console.log(responce)
+            console.log(responce.body)
         }
     }
     onStarHoverOne() {
